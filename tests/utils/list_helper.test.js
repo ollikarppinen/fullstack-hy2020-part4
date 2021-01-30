ba74bbc7cf3d4c0969e0026ae2838dbf272ae58b
Olli Karppinen <olli.karppinen@relex.fi>
2021-01-30 12:43:10 +0200

    Adds 4.4 solution

--- tests/utils/list_helper.test.js
+++ tests/utils/list_helper.test.js
@@ -1,8 +1,35 @@
-const listHelper = require("../../utils/list_helper")
+const { dummy, totalLikes } = require("../../utils/list_helper")
 
-test("dummy returns one", () => {
-  const blogs = []
+describe("totalLikes", () => {
+  describe("without blogs", () => {
+    test("returns 0", () => expect(totalLikes([])).toBe(0))
+  })
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+  describe("with blog", () => {
+    describe("without likes", () => {
+      test("returns 0", () => expect(totalLikes([{ foo: 1 }])).toBe(0))
+    })
+
+    describe("without 0 likes", () => {
+      test("returns 0", () => expect(totalLikes([{ likes: 0 }])).toBe(0))
+    })
+
+    describe("without 1 like", () => {
+      test("returns 1", () => expect(totalLikes([{ likes: 1 }])).toBe(1))
+    })
+  })
+
+  describe("with multiple blogs", () => {
+    test("returns sum of likes", () =>
+      expect(totalLikes([{ likes: 1 }, { likes: 2 }])).toBe(3))
+  })
+})
+
+describe("dummy", () => {
+  test("returns 1", () => {
+    const blogs = []
+
+    const result = dummy(blogs)
+    expect(result).toBe(1)
+  })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,10 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) =>
+  blogs.reduce((sum, { likes = 0 }) => sum + likes, 0)
+
 module.exports = {
   dummy,
+  totalLikes,
 }
