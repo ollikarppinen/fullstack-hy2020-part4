06f2bd87414bc41fcb623565ba65f467470ca414
Olli Karppinen <olli.karppinen@relex.fi>
2021-01-30 12:57:23 +0200

    Adds 4.5 solution

--- tests/utils/list_helper.test.js
+++ tests/utils/list_helper.test.js
@@ -1,4 +1,4 @@
-const { dummy, totalLikes } = require("../../utils/list_helper")
+const { dummy, totalLikes, favouriteBlog } = require("../../utils/list_helper")
 
 describe("totalLikes", () => {
   describe("without blogs", () => {
@@ -10,11 +10,11 @@ describe("totalLikes", () => {
       test("returns 0", () => expect(totalLikes([{ foo: 1 }])).toBe(0))
     })
 
-    describe("without 0 likes", () => {
+    describe("with 0 likes", () => {
       test("returns 0", () => expect(totalLikes([{ likes: 0 }])).toBe(0))
     })
 
-    describe("without 1 like", () => {
+    describe("with 1 like", () => {
       test("returns 1", () => expect(totalLikes([{ likes: 1 }])).toBe(1))
     })
   })
@@ -22,6 +22,46 @@ describe("totalLikes", () => {
   describe("with multiple blogs", () => {
     test("returns sum of likes", () =>
       expect(totalLikes([{ likes: 1 }, { likes: 2 }])).toBe(3))
+  })
+})
+
+describe("favouriteBlog", () => {
+  describe("without blogs", () => {
+    test("returns undefined", () => expect(favouriteBlog([])).toBe(undefined))
+  })
+
+  describe("with blog", () => {
+    describe("without likes", () => {
+      const blog = { foo: 1 }
+      test("returns blog", () => expect(favouriteBlog([blog])).toEqual(blog))
+    })
+
+    describe("with 1 like", () => {
+      const blog = { likes: 1 }
+      test("returns blog", () => expect(favouriteBlog([blog])).toEqual(blog))
+    })
+  })
+
+  describe("with multiple blogs", () => {
+    const blog = { likes: 1 }
+
+    describe("with one blog having most likes", () => {
+      const blogWithMostLikes = { likes: 2 }
+
+      test("returns the blog with most likes", () =>
+        expect(favouriteBlog([blog, blogWithMostLikes])).toEqual(
+          blogWithMostLikes
+        ))
+    })
+
+    describe("with multiple blogs having most likes", () => {
+      const blogWithTiedMostLikes = { title: "blogWithTiedMostLikes", likes: 1 }
+
+      test("returns the first blog with most likes", () =>
+        expect(favouriteBlog([blogWithTiedMostLikes, blog])).toEqual(
+          blogWithTiedMostLikes
+        ))
+    })
   })
 })
 
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -5,7 +5,17 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) =>
   blogs.reduce((sum, { likes = 0 }) => sum + likes, 0)
 
+const favouriteBlog = (blogs) =>
+  blogs.reduce(
+    (blogWithMostLikes, blog) =>
+      blogWithMostLikes && blogWithMostLikes.likes >= blog.likes
+        ? blogWithMostLikes
+        : blog,
+    undefined
+  )
+
 module.exports = {
   dummy,
   totalLikes,
+  favouriteBlog,
 }
