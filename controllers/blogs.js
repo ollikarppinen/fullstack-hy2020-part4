571faebceaa1586f397fa1ae451999605f02e1a2
Olli Karppinen <olli.karppinen@relex.fi>
2021-02-01 21:31:14 +0200

    Adds 4.11 solution

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -7,7 +7,7 @@ blogsRouter.get("/", async (request, response) => {
 })
 
 blogsRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body)
+  const blog = new Blog({ likes: 0, ...request.body })
 
   await blog.save()
   response.status(201).json(blog)
--- tests/controllers/blogs.test.js
+++ tests/controllers/blogs.test.js
@@ -56,23 +56,51 @@ describe("create", () => {
     likes: "123456",
   }
   describe("with 2 blogs", () => {
-    test("returns json", async () => {
-      await api
-        .post("/api/blogs")
-        .send(newBlog)
-        .expect(201)
-        .expect("Content-Type", /application\/json/)
+    describe("with all parameters", () => {
+      test("returns json", async () => {
+        await api
+          .post("/api/blogs")
+          .send(newBlog)
+          .expect(201)
+          .expect("Content-Type", /application\/json/)
+      })
+
+      test("persists blog in db", async () => {
+        await api.post("/api/blogs").send(newBlog)
+        const blogs = await Blog.find(newBlog)
+        expect(blogs).toHaveLength(1)
+      })
+
+      test("responds with correct body", async () => {
+        const response = await api.post("/api/blogs").send(newBlog)
+        expect(response.body).toMatchObject({ ...newBlog, likes: 123456 })
+      })
     })
 
-    test("persists blog in db", async () => {
-      await api.post("/api/blogs").send(newBlog)
-      const blogs = await Blog.find(newBlog)
-      expect(blogs).toHaveLength(1)
-    })
+    describe("without like parameter", () => {
+      const newBlog = {
+        title: "newBlog",
+        author: "newAuthor",
+        url: "newUrl",
+      }
+      test("returns json", async () => {
+        await api
+          .post("/api/blogs")
+          .send(newBlog)
+          .expect(201)
+          .expect("Content-Type", /application\/json/)
+      })
 
-    test("responds with correct body", async () => {
-      const response = await api.post("/api/blogs").send(newBlog)
-      expect(response.body).toMatchObject({ ...newBlog, likes: 123456 })
+      test("persists blog in db", async () => {
+        await api.post("/api/blogs").send(newBlog)
+        const blogs = await Blog.find(newBlog)
+        expect(blogs).toHaveLength(1)
+      })
+
+      test("responds with correct body with 0 likes", async () => {
+        const response = await api.post("/api/blogs").send(newBlog)
+        expect(response.body).toMatchObject({ ...newBlog, likes: 0 })
+      })
     })
   })
 })
