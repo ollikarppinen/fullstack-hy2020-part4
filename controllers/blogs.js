b4836e419033730868105617f7299a9402e1c831
Olli Karppinen <olli.karppinen@relex.fi>
2021-02-03 22:16:23 +0200

    Adds 4.19 solution

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
 const User = require("../models/user")
+const jwt = require("jsonwebtoken")
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization")
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({})
@@ -8,7 +17,13 @@ blogsRouter.get("/", async (request, response) => {
 })
 
 blogsRouter.post("/", async (request, response) => {
-  const user = await User.findOne({})
+  const token = getTokenFrom(request)
+  console.log("token", token)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" })
+  }
+  const user = await User.findById(decodedToken.id)
   const blog = new Blog({ likes: 0, ...request.body, user: user._id })
 
   const savedBlog = await blog.save()
