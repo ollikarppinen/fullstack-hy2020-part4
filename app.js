e1e905f5a61e8388fa0f3394a574b7e25680b359
Olli Karppinen <olli.karppinen@relex.fi>
2021-02-23 21:51:44 +0200

    Adds thingies till 5.20

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const config = require("./utils/config")
-const express = require("express")
-require("express-async-errors")
-const app = express()
-const cors = require("cors")
-const blogsRouter = require("./controllers/blogs")
-const usersRouter = require("./controllers/users")
-const loginRouter = require("./controllers/login")
-const middleware = require("./utils/middleware")
-const logger = require("./utils/logger")
-const mongoose = require("mongoose")
+const config = require("./utils/config");
+const express = require("express");
+require("express-async-errors");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+const testingRouter = require("./controllers/testing");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
 
-logger.info("connecting to", config.MONGODB_URI)
+logger.info("connecting to", config.MONGODB_URI);
 
 mongoose
   .connect(config.MONGODB_URI, {
@@ -20,23 +21,24 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => {
-    logger.info("connected to MongoDB")
+    logger.info("connected to MongoDB");
   })
   .catch((error) => {
-    logger.error("error connecting to MongoDB:", error.message)
-  })
+    logger.error("error connecting to MongoDB:", error.message);
+  });
 
-app.use(cors())
-app.use(express.static("build"))
-app.use(express.json())
-app.use(middleware.requestLogger)
-app.use(middleware.tokenExtractor)
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
 
-app.use("/api/blogs", blogsRouter)
-app.use("/api/users", usersRouter)
-app.use("/api/login", loginRouter)
+app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
+app.use("/api/testing", testingRouter);
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-module.exports = app
+module.exports = app;
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,52 +1,52 @@
-const blogsRouter = require("express").Router()
-const Blog = require("../models/blog")
-const User = require("../models/user")
-const jwt = require("jsonwebtoken")
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({})
-  response.json(blogs)
-})
+  const blogs = await Blog.find({});
+  response.json(blogs);
+});
 
 blogsRouter.post("/", async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
   if (!request.token || !decodedToken.id) {
-    return response.status(401).json({ error: "token missing or invalid" })
+    return response.status(401).json({ error: "token missing or invalid" });
   }
-  const user = await User.findById(decodedToken.id)
-  const blog = new Blog({ likes: 0, ...request.body, user: user._id })
+  const user = await User.findById(decodedToken.id);
+  const blog = new Blog({ likes: 0, ...request.body, user: user._id });
 
-  const savedBlog = await blog.save()
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
 
-  response.status(201).json(blog)
-})
+  response.status(201).json(blog);
+});
 
 blogsRouter.delete("/:id", async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
   if (!request.token || !decodedToken.id) {
-    return response.status(401).json({ error: "token missing or invalid" })
+    return response.status(401).json({ error: "token missing or invalid" });
   }
-  const user = await User.findById(decodedToken.id)
-  const blog = await Blog.findById(request.params.id)
+  const user = await User.findById(decodedToken.id);
+  const blog = await Blog.findById(request.params.id);
 
   if (blog.user.toString() !== user._id.toString()) {
-    return response.status(403).json({ error: "only creator can delete blog" })
+    return response.status(403).json({ error: "only creator can delete blog" });
   }
 
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
-})
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
 
 blogsRouter.put("/:id", async (request, response) => {
-  const { title, author, url, likes } = request.body
+  const { title, author, url, likes } = request.body;
   const blog = await Blog.findByIdAndUpdate(
     { _id: request.params.id },
     { title, author, url, likes },
-    {}
-  )
-  response.status(200).json(blog)
-})
+    { new: true }
+  );
+  response.status(200).json(blog);
+});
 
-module.exports = blogsRouter
+module.exports = blogsRouter;
